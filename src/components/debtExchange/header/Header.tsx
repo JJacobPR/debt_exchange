99f06f1aac2408b64aa7ac62f731e4c1ea74fab6
Jakub Janicki <jjanicki08@gmail.com>
2025-03-24 15:16:44 +0100

    Added search

--- src/components/debtExchange/header/Header.tsx
+++ src/components/debtExchange/header/Header.tsx
@@ -1,14 +1,24 @@
 import { useTableContext } from "@store/tableContext";
 import styles from "./Header.module.scss";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Header = () => {
   const [searchPhrase, setSearchPhrase] = useState<string>("");
   const { searchRows } = useTableContext();
+  const [warning, setWarning] = useState<boolean>(false);
 
   const submitSearchForm = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    searchRows(searchPhrase);
+    if (searchPhrase.length < 3) {
+      setWarning(true);
+      const timer = setTimeout(() => {
+        setWarning(false);
+      }, 3000);
+
+      return () => {
+        clearTimeout(timer);
+      };
+    } else searchRows(searchPhrase);
   };
 
   return (
@@ -19,6 +29,7 @@ const Header = () => {
           <input type="text" onChange={(e) => setSearchPhrase(e.target.value)} />
           <button type="submit">Szukaj</button>
         </form>
+        <p className={`${styles["warning"]} ${warning && styles["warning-fade-in"]}`}>Wprowadź minimalnie 3 znaki</p>
       </header>
     </div>
   );
